fb9346dbd4afd09bd57f104a793ea399fda31b1d
Musab Alhuthali <mosab-888@hotmail.com>
2025-09-19 05:51:47 +0000

    Complete all calculations for the calculate function

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,16 @@
 import numpy as np
 
-def calculate(list):
-
-
-
-
-    return calculations
+def calculate(l):
+    if len(l) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    m = np.array(l).reshape(3,3)
+    d = {
+        "mean": [m.mean(axis=0).tolist(), m.mean(axis=1).tolist(), m.mean().tolist()],
+        "variance": [m.var(axis=0).tolist(), m.var(axis=1).tolist(), m.var().tolist()],
+        "standard deviation": [m.std(axis=0).tolist(), m.std(axis=1).tolist(), m.std().tolist()],
+        "max": [m.max(axis=0).tolist(), m.max(axis=1).tolist(), m.max().tolist()],
+        "min": [m.min(axis=0).tolist(), m.min(axis=1).tolist(), m.min().tolist()],
+        "sum": [m.sum(axis=0).tolist(), m.sum(axis=1).tolist(), m.sum().tolist()]
+    }
+    return d
